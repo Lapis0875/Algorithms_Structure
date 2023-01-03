2d5123726f51e2f6845845edaa093dbd7e0b2a74
Lapis0875 <lapis0875@gmail.com>
2023-01-03 18:02:25 +0900

    Fixed q3.c

--- Structure/Array/q3-1.c
+++ Structure/Array/q3-1.c
@@ -2,36 +2,48 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void print_array(int N, int arr[N][N]);
+void print_array(int N, int **arr);
 
 int main(void)
 {
     int N = 0;
     scanf("%d", &N);
 
-    int (*arr)[N];
-    arr = (int(*)[N]) malloc((N * N) * sizeof(int));
+    int **arr;
+    arr = (int**) malloc(N * sizeof(int*));
     if (arr == NULL)
         return -1;
 
     for (int i = 0; i < N; i ++)
     {
+        arr[i] = malloc(N * sizeof(int));
         if (i % 2)  // backward
         {
             for (int j = N-1; j >= 0; j--)
+            {
                 arr[i][j] = (i + 1) * N - j;
+                #ifdef DEBUG
+                    printf("> value (%d, %d) = %d\n", i, j, arr[i][j]);
+                #endif
+
+            }
         }
         else    // forward
         {
             for (int j = 0; j < N; j++)
+            {
                 arr[i][j] = i * N + (j + 1);
+                #ifdef DEBUG
+                    printf("> value (%d, %d) = %d\n", i, j, arr[i][j]);
+                #endif
+            }
         }
     }
     print_array(N, arr);
     return 0;
 }
 
-void print_array(int N, int arr[N][N])
+void print_array(int N, int **arr)
 {
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
